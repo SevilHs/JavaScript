07f272d519ea0ed944139b5dd2f1bf292e04514e
Sevil Huseynova <126726212+SevilHs@users.noreply.github.com>
2023-04-30 02:04:21 +0400

    Update script.js

--- Homework/14.Loop and if,else statement/script.js
+++ Homework/14.Loop and if,else statement/script.js
@@ -1,85 +1,85 @@
-// let с=calcAge(65) , q=calcAge(0) , y=calcAge(20) ;
+let с=calcAge(65) , q=calcAge(0) , y=calcAge(20) ;
 
-// function calcAge(age) {
-//   return ( age * 365
-//  )
-// }    
+function calcAge(age) {
+  return ( age * 365
+ )
+}    
 
-// console.log(с);
-// console.log(q);
-// console.log(y);
-
-
-// let all = 0;
-// for (let i = 1; i < 10; i++) {
-//   if (i % 2 == 0) {
-//     console.log(i);
-//     all +=i;
-//   }
-// }
-// console.log(all);
-
-// let mul = 1;
-// for (let a = 1; a < 17; a++) {
-//   if (a % 2 != 0) {
-//     console.log(a);
-//     mul*=a;
-//   }
-// }
-// console.log(mul);
-
-// let x = 1;
-// for (let b = 10; b <= 18; b++) {
-//   console.log(b);
-//   x *=b;
-// }
-// console.log(x);
+console.log(с);
+console.log(q);
+console.log(y);
 
 
+let all = 0;
+for (let i = 1; i < 10; i++) {
+  if (i % 2 == 0) {
+    console.log(i);
+    all +=i;
+  }
+}
+console.log(all);
 
-// let k=1
+let mul = 1;
+for (let a = 1; a < 17; a++) {
+  if (a % 2 != 0) {
+    console.log(a);
+    mul*=a;
+  }
+}
+console.log(mul);
 
-// for (let j = 17; j <=30; j++) {
-//     if (j===19 || j===20) {
-//         continue
-//     }
-//     console.log(j);
-//     k*=j
-// }
-// console.log(k);
+let x = 1;
+for (let b = 10; b <= 18; b++) {
+  console.log(b);
+  x *=b;
+}
+console.log(x);
 
 
 
+let k=1
 
-// let sum=0
-// let v=1
-// while (v <= 30) {
-//   if (v%2==0) {
-//     console.log(v);
-//     sum+=v
-//   }v++;
-// }
-// console.log(sum);
-
-// let sum2=0
-// let n=1
-// while (n <= 30) {
-//   if (n%2!=0) {
-//     console.log(n);
-//     sum2+=n
-//   }n++;
-// }
-// console.log(sum2);
-
-// console.log(sum-sum2);
+for (let j = 17; j <=30; j++) {
+    if (j===19 || j===20) {
+        continue
+    }
+    console.log(j);
+    k*=j
+}
+console.log(k);
 
 
-// for (let w = 1; w <=16; w++) {
-//     if (w%2==0) {
-//         console.log(`${w} ${w*w}`);
-//     }
+
+
+let sum=0
+let v=1
+while (v <= 30) {
+  if (v%2==0) {
+    console.log(v);
+    sum+=v
+  }v++;
+}
+console.log(sum);
+
+let sum2=0
+let n=1
+while (n <= 30) {
+  if (n%2!=0) {
+    console.log(n);
+    sum2+=n
+  }n++;
+}
+console.log(sum2);
+
+console.log(sum-sum2);
+
+
+for (let w = 1; w <=16; w++) {
+    if (w%2==0) {
+        console.log(`${w} ${w*w}`);
+    }
     
-// }
+}
 
 // let snm="Front-end Developer"
 // for (let f = 0; f <=snm.length; f++) {
