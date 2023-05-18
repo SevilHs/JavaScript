a05100d95ae7165ab78d9f00331f793820217157
Sevil Huseynova <126726212+SevilHs@users.noreply.github.com>
2023-05-18 02:59:36 -0700

    Add files via upload

---
+++ Classwork/27.recursion/script.js
@@ -0,0 +1,38 @@
+// Task 1
+
+function func1(num){
+    let result=1
+    for (let i = 1; i <= num; i++){
+        result= result*i
+    }
+    return   result
+}
+
+console.log(func1(4));
+console.log(func1(5));
+console.log(func1(0));
+console.log(func1(-1));
+
+
+// Task 2
+function func2(num) {
+    let result=1
+    for (let i = 1; i <=num; i=i+2) {
+        result=result*i
+    }
+    return result
+}
+
+console.log(func2(5));
+console.log(func2(9));
+console.log(func2(1));
+
+// Task 3
+let arr1=[]
+function sumArray(arr) {
+    let sum=0
+    // return (arr.flat(Infinity)).reduce((a,b)=>a+b)
+   return arr.reduce((a,b)=>a+b,0).split(',').reduce((a,b)=>a + Number(b),0)
+}
+
+console.log(sumArray([4,[2,[1]],8]));
