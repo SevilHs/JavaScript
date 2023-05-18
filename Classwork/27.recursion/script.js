d8ce22f3524714a81b1d159f5b3780a50b3e777c
SevilHs <7l2zfk3@code.edu.az>
2023-05-18 15:45:20 +0400

    update script.js

--- Classwork/27.recursion/script.js
+++ Classwork/27.recursion/script.js
@@ -1,11 +1,12 @@
 // Task 1
 
-function func1(num){
-    let result=1
-    for (let i = 1; i <= num; i++){
-        result= result*i
-    }
-    return   result
+function func1(num) {
+//   let result = 1;
+//   for (let i = 1; i <= num; i++) {
+//     result = result * i;
+//   }
+//   return result;
+  return num>0 ? num* func1(num-1): 1
 }
 
 console.log(func1(4));
@@ -13,14 +14,14 @@ console.log(func1(5));
 console.log(func1(0));
 console.log(func1(-1));
 
-
 // Task 2
 function func2(num) {
-    let result=1
-    for (let i = 1; i <=num; i=i+2) {
-        result=result*i
-    }
-    return result
+//   let result = 1;
+//   for (let i = 1; i <= num; i = i + 2) {
+//     result = result * i;
+//   }
+//   return result;
+return num>1 ? num*func2(num-2) : 1
 }
 
 console.log(func2(5));
@@ -28,11 +29,14 @@ console.log(func2(9));
 console.log(func2(1));
 
 // Task 3
-let arr1=[]
+let arr1 = [];
 function sumArray(arr) {
-    let sum=0
-    // return (arr.flat(Infinity)).reduce((a,b)=>a+b)
-   return arr.reduce((a,b)=>a+b,0).split(',').reduce((a,b)=>a + Number(b),0)
+  // return (arr.flat(Infinity)).reduce((a,b)=>a+b)
+  //    return arr.reduce((a,b)=>a+b,0).split(',').reduce((a,b)=>a + Number(b),0)
+  return arr.reduce(
+    (acc, curr) => (Array.isArray(curr) ? acc + sumArray(curr) : acc + curr),
+    0
+  );
 }
 
-console.log(sumArray([4,[2,[1]],8]));
+console.log(sumArray([4, [2, [1]], 8]));
